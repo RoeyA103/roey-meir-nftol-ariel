afa4887a4814ed43bbaef7f09832d54065cff048
meyir moshe <mma088651259@gmail.com>
2026-01-29 17:35:24 +0200

    create crud for database

--- app/database.py
+++ app/database.py
@@ -1,2 +1,39 @@
-"hi"
-print("ssssssssss")
+from models import MissionCreate, MissionResponse
+
+
+class Database:
+    employees: list = []
+    missions: list = []
+
+    def add_mission(self,mission:MissionCreate)-> MissionCreate:
+        self.missions.append(mission)
+        return mission
+    def get_all_missions(self)-> list[MissionCreate]:
+        return self.missions
+
+    def get_mission_by_id(self,mission_id:str)->MissionResponse | None:
+        for mission in self.missions:
+            if mission.id == mission_id:
+                return mission
+        return None
+
+    def get_missions_by_employee(self, emp_id: str) -> list[MissionResponse] | None:
+        for mission in self.missions:
+            if mission.assigned_to == emp_id:
+                return mission
+        return None
+
+    def update_mission(self,mission_id:str, data:dict):
+        mission = self.get_mission_by_id(mission_id)
+        if not mission:
+            return None
+        for key, value in data.items():
+            setattr(mission,key,value)
+        return mission
+
+    def delete_mission(self, mission_id: str) -> bool:
+        mission = self.get_mission_by_id(mission_id)
+        if not mission:
+            return False
+        self.missions.remove(mission)
+        return True
