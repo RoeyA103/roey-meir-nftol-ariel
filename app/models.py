9696520df62a4239fb77fccea88496316d480637
Meyir <mma088651259@gmail.com>
2026-01-29 17:18:58 +0200

    Feature/mission routes (#6)
    * f
    
    * Describe what you changed
    
    * create missings models
    
    ---------
    
    Co-authored-by: roey averbuch <roey6922@gmail.com>

--- app/models.py
+++ app/models.py
@@ -1,4 +1,31 @@
 
+from datetime import datetime
+from pydantic import BaseModel
+from  typing import Optional
+
+
+class MissionBase(BaseModel):  #שדות בסיסים של מחלקת משימה
+    title: str
+    assigned_to: str
+    status: str
+    priority: str
+    deadline: str
+
+class MissionCreate(MissionBase):   #יצירת משימה
+    id: str
+
+class MissionUpdate(BaseModel):   #עדכון משימה
+    title: Optional[str] = None
+    assigned_to: Optional[str] = None
+    status: Optional[str] = None
+    priority: Optional[str] = None
+    deadline: Optional[str] = None
+
+class MissionResponse(MissionBase):    #השרת  מחזיר תגובה אחרי יצירה וכו
+    id: str
+    created_at: datetime
+
+
 def get_employee_statistics() -> dict:
     total_employees = len(emplyees)
     by_office = {}
@@ -108,3 +135,4 @@ class EmployeeResponse(EmployeeBase):
     id: str
     created_at: datetime
     updated_at: datetime
+
