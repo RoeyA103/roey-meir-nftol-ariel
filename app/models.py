42912508a504b331d445bdf261f94f7d46f53089
roey_averbuch <roey6922@gmail.com>
2026-01-29 17:21:08 +0200

    a

--- app/models.py
+++ app/models.py
@@ -3,6 +3,29 @@ from datetime import datetime
 from pydantic import BaseModel
 from  typing import Optional
 
+class EmployeeBase(BaseModel):
+    first_name: str
+    last_name: str
+    office_name: str
+    job_title: str
+
+
+class EmployeeCreate(EmployeeBase):
+    id: str
+
+
+class EmployeeUpdate(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    office_name: Optional[str] = None
+    job_title: Optional[str] = None
+
+
+
+class EmployeeResponse(EmployeeBase):
+    id: str
+    created_at: datetime
+    updated_at: datetime
 
 class MissionBase(BaseModel):  #שדות בסיסים של מחלקת משימה
     title: str
@@ -24,6 +47,7 @@ class MissionUpdate(BaseModel):   #עדכון משימה
 class MissionResponse(MissionBase):    #השרת  מחזיר תגובה אחרי יצירה וכו
     id: str
     created_at: datetime
+
 
 
 def get_employee_statistics() -> dict:
@@ -107,32 +131,4 @@ def get_employee_workload(employees, missions) -> list[dict]:
 
     return workload
   
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
-
-
-class EmployeeBase(BaseModel):
-    first_name: str
-    last_name: str
-    office_name: str
-    job_title: str
-
-
-class EmployeeCreate(EmployeeBase):
-    id: str
-
-
-class EmployeeUpdate(BaseModel):
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    office_name: Optional[str] = None
-    job_title: Optional[str] = None
-
-
-
-class EmployeeResponse(EmployeeBase):
-    id: str
-    created_at: datetime
-    updated_at: datetime
 
